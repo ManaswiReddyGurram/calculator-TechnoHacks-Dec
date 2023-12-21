f86592a56b45cff942d1b66a32e077b5684bc780
ManaswiReddyGurram <131012655+ManaswiReddyGurram@users.noreply.github.com>
2023-12-21 22:07:53 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+
+def on_button_click(value):
+    current_text = entry.get()
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, current_text + str(value))
+
+def clear_entry():
+    entry.delete(0, tk.END)
+
+def calculate():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, str(result))
+    except Exception as e:
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, "Error")
+
+# Create the main window
+root = tk.Tk()
+root.title("Calculator")
+
+# Entry widget to display input and results
+entry = tk.Entry(root, width=20, font=('Arial', 16), bd=5, insertwidth=4)
+entry.grid(row=0, column=0, columnspan=4)
+
+# Buttons
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+'
+]
+
+row_val = 1
+col_val = 0
+
+for button in buttons:
+    tk.Button(root, text=button, width=5, height=2,
+              command=lambda button=button: on_button_click(button) if button != '=' else calculate()).grid(row=row_val, column=col_val)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
+
+# Clear button
+tk.Button(root, text='C', width=5, height=2, command=clear_entry).grid(row=row_val, column=col_val)
+
+# Run the Tkinter main loop
+root.mainloop()
